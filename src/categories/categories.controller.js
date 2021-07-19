e88bf59685ba471b6ba0e6453324ec6135d21ee4
Nick Terns <ternsna11@gmail.com>
2021-07-19 13:39:25 -0500

    async functions added

--- src/categories/categories.controller.js
+++ src/categories/categories.controller.js
@@ -1,16 +1,21 @@
 const categoriesService = require("./categories.service");
 
-function list(req, res, next) {
-  categoriesService
-    .list()
-    .then((data) => res.json({ data }))
-    .catch(next);
+async function list(res, next) {
+  const data = await categoriesService.list();
+  res.json({ data })
 }
+// The categoriesService.list() function executes a Knex query, which is an asynchronous operation. Using the await keyword before categoriesService.list() forces the execution of the code to pause on that line until that asynchronous operation is finished. Once it is, the resolved response is stored in categories. Because the list() function contains a function that uses await, you must add the async keyword in front of the list() function. Otherwise, your code won't work properly.
+
 
 module.exports = {
   list,
 };
-
+// function list(req, res, next) {
+//   categoriesService
+//     .list()
+//     .then((data) => res.json({ data }))
+//     .catch(next);
+// }
 // const categoriesService = require("./categories.service"); requires the service object that you created in the previous step and assigns it to categoriesService.
 
 // You can then access the methods on the service object to perform CRUD operations on a table (for example, categoriesService.list()). Chaining then() to categoriesService.list()executes the Knex query. Chaining catch(next) onto the promise will call next() passing in the error. If the Knex promise does not have a catch(next) at the end, it will not correctly handle errors that occur during when running the query.
--- src/products/products.controller.js
+++ src/products/products.controller.js
@@ -1,36 +1,48 @@
 const productsService = require("./products.service");
 
-function productExists(req, res, next) {
-  productsService
-    .read(req.params.productId)
-    // Chaining then() to productsService.read(productId) will execute the Knex query that you defined previously to retrieve a product given an id. The query returns a promise, which is handled in the then() function.
-    .then((product) => {
-      if (product) {
-        res.locals.product = product;
-        return next();
-      }
-      // If the product exists, it is stored in res.locals.product so that it can be readily accessed in the rest of the middleware pipeline. Otherwise, next() is called with an error
-      next({ status: 404, message: `Product cannot be found.` });
-    })
-    .catch(next);
+async function productExists(req, res, next) {
+  const data = await productsService.read(req.params.productId);
+  if (product) {
+    res.locals.product = product;
+    return next();
+  }
+  next({ status: 404, message: `Product cannot be found.` });
 }
-// 
 
+async function list(req, res, next){
+  const data = await productsService.list()
+  res.json({ data })
+}
 
 function read(req, res) {
   const { product: data } = res.locals;
   res.json({ data });
 }
 
-function list(req, res, next) {
-  productsService
-    .list()
-    .then((data) => res.json({ data }))
-    .catch(next);
-}
+
 
 module.exports = {
   read: [productExists, read],
   list,
 };
 
+//function productExists(req, res, next) {
+// .read(req.params.productId)
+// .then((product) => {
+//   if (product) {
+//     res.locals.product = product;
+//     return next();
+//   }
+// Chaining then() to productsService.read(productId) will execute the Knex query that you defined previously to retrieve a product given an id. The query returns a promise, which is handled in the then() function.
+//   // If the product exists, it is stored in res.locals.product so that it can be readily accessed in the rest of the middleware pipeline. Otherwise, next() is called with an error
+//   next({ status: 404, message: `Product cannot be found.` });
+// })
+// .catch(next);
+
+
+// function list(req, res, next) {
+//   productsService
+//     .list()
+//     .then((data) => res.json({ data }))
+//     .catch(next);
+// }
